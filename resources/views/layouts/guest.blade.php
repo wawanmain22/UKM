51475f99c25f71bdb4d867787aedd84d45ea1228
wawanmain22 <wawanmain22@gmail.com>
2025-02-06 06:57:15 +0700

    Re-enable Vite asset compilation in layout files

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Guest Portal</title>
-    {{-- @vite(['resources/css/app.css', 'resources/js/app.jsx']) --}}
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
 </head>
 <body class="bg-gray-100 text-gray-800">
     <!-- Navbar -->
